eb71e4eb7177dba0ef5e5457b20f4a5e3d94051d
HeribertoGM <HeribertoGM1999@outlook.com>
2021-10-01 21:01:25 -0500

    ADD - Parser v1, test1/2

--- Lexer_Parser.py
+++ Lexer_Parser.py
@@ -7,7 +7,7 @@ import ply.yacc as yacc
 # -----------------------
 
 tokens = [
-    # palabras reservadas
+	# palabras reservadas
 	'PROGRAM',
 	'MAIN',
 	'LET',
@@ -103,15 +103,15 @@ t_TIMES = r'\*'
 t_DIVIDE = r'\/'
 t_MODULE = r'\%'
 
-t_ignore = '[ \t\n]'
+t_ignore = ' \t\n'
 
 def t_PROGRAM(t):
 	r'program'
 	return t
 
 def t_MAIN(t):
-    r'main'
-    return t
+	r'main'
+	return t
 
 def t_LET(t):
 	r'let'
@@ -126,32 +126,32 @@ def t_FLOAT(t):
 	return t
 
 def t_CHAR(t):
-    r'char'
-    return t
+	r'char'
+	return t
 
 def t_STRING(t):
-    r'string'
-    return t
+	r'string'
+	return t
 
 def t_FUNCTION(t):
-    r'function'
-    return t
+	r'function'
+	return t
 
 def t_VOID(t):
-    r'void'
-    return t
+	r'void'
+	return t
 
 def t_RETURN(t):
-    r'return'
-    return t
+	r'return'
+	return t
 
 def t_READ(t):
-    r'read'
-    return t
+	r'read'
+	return t
 
 def t_WRITE(t):
-    r'write'
-    return t
+	r'write'
+	return t
 
 def t_IF(t):
 	r'if'
@@ -162,16 +162,16 @@ def t_ELSE(t):
 	return t
 
 def t_WHILE(t):
-    r'while'
-    return t
+	r'while'
+	return t
 
 def t_FOR(t):
-    r'for'
-    return t
+	r'for'
+	return t
 
 def t_TO(t):
-    r'to'
-    return t
+	r'to'
+	return t
 
 def t_ID(t):
 	r'[a-zA-Z_][a-zA-Z0-9_]*'
@@ -204,6 +204,7 @@ def t_error(t):
 
 lexer = lex.lex()
 
+
 # inp = str(input(">> "))
 # while inp != "":
 # 	lexer.input(inp)
@@ -215,3 +216,309 @@ lexer = lex.lex()
 
 # 	inp = str(input(">> "))
 
+
+# -----------------------
+# Parser
+# -----------------------
+
+# Programa
+def p_programa(p):
+	'''
+	programa : PROGRAM ID SEMICOLON declaracion funcion MAIN O_PARENTHESIS C_PARENTHESIS bloque
+	'''
+	print("call programa")
+
+# Bloque
+def p_bloque(p):
+	'''
+	bloque : O_CBRACKET bloque_prime C_CBRACKET
+	'''
+	print("call bloque")
+
+def p_bloque_prime(p):
+	'''
+	bloque_prime : estatuto bloque_prime
+				 | epsilon
+	'''
+	print("call bloque_prime")
+
+# Declaracion
+def p_declaracion(p):
+	'''
+	declaracion : declaracion_base
+				| declaracion_base declaracion
+	'''
+	print("call declaracion")
+
+def p_declaracion_base(p):
+	'''
+	declaracion_base : LET declaracion_prime COLON tipo SEMICOLON
+	'''
+	print("call declaracion_base")
+
+def p_declaracion_prime(p):
+	'''
+	declaracion_prime : variable
+					  | variable COMMA declaracion_prime
+	'''
+	print("call declaracion_prime")
+
+# Tipo
+def p_tipo(p):
+	'''
+	tipo : INT
+		 | FLOAT
+		 | CHAR
+		 | STRING
+	'''
+	print("call tipo")
+
+# Funcion
+def p_funcion(p):
+	'''
+	funcion : funcion_base
+			  | funcion_base funcion
+	'''
+	print("call funcion")
+
+def p_funcion_base(p):
+	'''
+	funcion_base : FUNCTION funcion_tipo ID O_PARENTHESIS funcion_prime C_PARENTHESIS declaracion bloque
+	'''
+	print("call funcion_base")
+
+def p_funcion_prime(p):
+	'''
+	funcion_prime : tipo ID
+				  | tipo ID COMMA funcion_prime
+	'''
+	print("call funcion_prime")
+
+def p_funcion_tipo(p):
+	'''
+	funcion_tipo : VOID
+ 				 | tipo
+	'''
+	print("call funcion_tipo")
+
+# Variable
+def p_variable(p):
+	'''
+	variable : ID O_ABRACKET variable_tipo C_ABRACKET O_ABRACKET variable_tipo C_ABRACKET O_ABRACKET variable_tipo C_ABRACKET
+			 | ID O_ABRACKET variable_tipo C_ABRACKET O_ABRACKET variable_tipo C_ABRACKET
+			 | ID O_ABRACKET variable_tipo C_ABRACKET
+			 | ID 
+	'''
+	print("call variable")
+
+def p_variable_tipo(p):
+	'''
+	variable_tipo : CTE_INT
+				  | exp
+	'''
+	print("call variable_tipo")
+
+# Estatuto
+def p_estatuto(p):
+	'''
+	estatuto : asignacion SEMICOLON
+			 | llamada SEMICOLON
+			 | retorno SEMICOLON
+			 | lectura SEMICOLON
+			 | escritura SEMICOLON
+			 | decision
+			 | while
+			 | for
+			 | expr SEMICOLON
+	'''
+	print("call estatuto")
+
+# Asignacion
+def p_asignacion(p):
+	'''
+	asignacion : variable ASSIGN expr
+	'''
+	print("call asignacion")
+
+# Llamada
+def p_llamada(p):
+	'''
+	llamada : ID O_PARENTHESIS llamada_prime C_PARENTHESIS
+	'''
+	print("call llamada")
+
+def p_llamada_prime(p):
+	'''
+	llamada_prime : exp
+				  | exp COMMA llamada_prime
+	'''
+	print("call llamada_prime")
+
+# Retorno
+def p_retorno(p):
+	'''
+	retorno : RETURN O_PARENTHESIS expr C_PARENTHESIS
+	'''
+	print("call retorno")
+
+# Lectura
+def p_lectura(p):
+	'''
+	lectura : READ O_PARENTHESIS lectura_prime C_PARENTHESIS
+	'''
+	print("call lectura")
+
+def p_lectura_prime(p):
+	'''
+	lectura_prime : variable
+				  | variable COMMA lectura_prime
+	'''
+	print("call lectura_prime")
+
+# Escritura
+def p_escritura(p):
+	'''
+	escritura : WRITE O_PARENTHESIS escritura_prime C_PARENTHESIS
+	'''
+	print("call escritura")
+
+def p_escritura_prime(p):
+	'''
+	escritura_prime : expr
+					| CTE_STRING
+					| expr COMMA escritura_prime
+					| CTE_STRING COMMA escritura_prime
+	'''
+	print("call escritura_prime")
+
+# Decision
+def p_decision(p):
+	'''
+	decision : IF O_PARENTHESIS expr C_PARENTHESIS bloque else
+	'''
+	print("call decision")
+
+def p_else(p):
+	'''
+	else : ELSE bloque
+		 | epsilon
+	'''
+	print("call else")
+
+# While
+def p_while(p):
+	'''
+	while : WHILE O_PARENTHESIS expr C_PARENTHESIS bloque
+	'''
+	print("call while")
+
+# For
+def p_for(p):
+	'''
+	for : FOR variable ASSIGN exp TO exp bloque
+	'''
+	print("call for")
+
+# Expr
+def p_expr(p):
+	'''
+	expr : or
+	'''
+	print("call expr")
+
+# Or
+def p_or(p):
+	'''
+	or : and
+	   | and OR or
+	'''
+	print("call or")
+
+# And
+def p_and(p):
+	'''
+	and : equal
+		| equal AND and
+	'''
+	print("call and")
+
+# Equal
+def p_equal(p):
+	'''
+	equal : compare
+		  | compare EQUAL compare
+		  | compare NOT_EQUAL compare
+	'''
+	print("call equal")
+
+# Compare
+def p_compare(p):
+	'''
+	compare : exp
+			| exp GREATER exp
+			| exp LESSER exp
+			| exp GREATER_EQUAL exp
+			| exp LESSER_EQUAL exp
+	'''
+	print("call compare")
+
+# Exp
+def p_exp(p):
+	'''
+	exp : termino
+		| termino PLUS exp
+		| termino MINUS exp
+	'''
+	print("call exp")
+
+# Termino
+def p_termino(p):
+	'''
+	termino : factor
+			| factor TIMES termino
+			| factor DIVIDE termino
+			| factor MODULE termino
+	'''
+	print("call termino")
+
+# Factor
+def p_factor(p):
+	'''
+	factor : variable
+		   | O_PARENTHESIS expr C_PARENTHESIS
+		   | llamada
+		   | cte
+		   | PLUS cte
+		   | MINUS cte
+	'''
+	print("call factor")
+
+# CTE
+def p_cte(p):
+	'''
+	cte : CTE_INT
+		| CTE_FLOAT
+	'''
+	print("call cte")
+
+# Epsilon
+def p_epsilon(p):
+	'''
+	epsilon :
+	'''
+	print("call epsilon")
+
+def p_error(p):
+	print("Parser error with: ", p)
+
+parser = yacc.yacc()
+
+program = None
+try:
+	s = "test2.txt"
+	with open(s, "r") as f:
+		program = f.read()
+except EOFError :
+    print("ERR")
+	
+parser.parse(program)
